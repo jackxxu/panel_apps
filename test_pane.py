00c106c6bc9347a16824cdfc411297e9a944d0b5
Jack Xu <jackxxu@gmail.com>
2024-06-01 14:05:37 -0400

    added the slider

--- test_pane.py
+++ test_pane.py
@@ -19,21 +19,29 @@ data = pd.DataFrame([
     ('Friday', 4), ('Saturday', 5), ('Sunday', 4)], columns=['Day', 'Wind Speed (m/s)']
 )
 
-fig = (
-    alt.Chart(data)
-    .mark_line(point=True)
-    .encode(
-        x="Day",
-        y=alt.Y("Wind Speed (m/s)", scale=alt.Scale(domain=(0, 10))),
-        tooltip=["Day", "Wind Speed (m/s)"],
-    )
-    .properties(width="container", height="container", title="Wind Speed Over the Week")
-)
 
-component = pn.panel(fig, sizing_mode="stretch_width", height=400)
-print(component)
+# create an integer slider
+slider = pn.widgets.IntSlider(name="Rows", start=1, end=7, step=1, value=7)
+
+
+def data_plot(rows=7):
+    print('1'*100)
+    fig = (
+        alt.Chart(data.head(rows))
+        .mark_line(point=True)
+        .encode(
+            x="Day",
+            y=alt.Y("Wind Speed (m/s)", scale=alt.Scale(domain=(0, 10))),
+            tooltip=["Day", "Wind Speed (m/s)"],
+        )
+        .properties(width="container", height="container", title="Wind Speed Over the Week")
+    )
+
+    return pn.panel(fig, sizing_mode="stretch_width", height=400)
+
 
 pn.Column(
-    component, 
+    slider,
+    pn.bind(data_plot, slider),
     pn.panel(data)
 ).servable()
